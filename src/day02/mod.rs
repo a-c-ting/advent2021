2f4250182fbd399eb927ca25d9fa85661652117f
a-c-ting <ating.collab.gmail.com>
2022-01-29 01:41:54 +0800

    Day 2 Part 2

--- src/day02/mod.rs
+++ src/day02/mod.rs
@@ -2,16 +2,20 @@ use std::fs;
 
 pub fn execute() {
     let input_path = ".\\input\\day02.txt";
-    println!("Reading file {}", input_path);
+    println!("Reading file {}\n", input_path);
 
     let file_contents = fs::read_to_string(input_path)
         .expect("Error in Reading File");
 
-    convert_input(file_contents);
+    let input_vector = file_contents.split_terminator('\n').collect::<Vec<_>>();
+
+    // Part 1
+    no_manual(&input_vector);
+    // Part 2
+    with_manual(&input_vector);
 }
 
-fn convert_input(file_contents: String) {
-    let input_vector = file_contents.split_terminator('\n').collect::<Vec<_>>();
+fn no_manual(input_vector: &Vec<&str>) {
     let (mut x, mut y) = (0, 0);
 
     for lines in input_vector {
@@ -24,6 +28,27 @@ fn convert_input(file_contents: String) {
         }
     }
 
-    println!("Coordinates: {} {}", x, y);
-    println!("Distance: {}", x*y);
+    println!("Distance: {}\n", x*y);
+}
+
+fn with_manual(input_vector: &Vec<&str>) {
+    let (mut x, mut aim, mut depth) = (0, 0, 0);
+
+    for lines in input_vector {
+        let commands = lines.split(" ").collect::<Vec<_>>();
+        match commands[0] {
+            "forward" => {
+                    let x_change = commands[1].parse::<i32>().unwrap();
+                    let depth_change = x_change*aim;
+
+                    x+=x_change;
+                    depth+=depth_change;
+                },
+            "down" => aim+=commands[1].parse::<i32>().unwrap(),
+            "up" => aim-=commands[1].parse::<i32>().unwrap(),
+            _ => unreachable!(),
+        }
+    }
+
+    println!("Correct Distance: {}\n", x*depth);
 }
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let target = &args[1].parse::<i32>().unwrap();
 
-    println!("Executing day {}\n", target);
+    println!("Executing day {}", target);
     match target {
         1 => day01::execute(),
         2 => day02::execute(),
